11a94a0f66eaed43781cd4767247ca1b8051be87
Aragao <jgabrielaragao@gmail.com>
2024-07-19 19:09:57 -0300

    visu form edit jogo

--- resources/views/app/jogo/edit.blade.php
+++ resources/views/app/jogo/edit.blade.php
@@ -4,10 +4,11 @@
 
 @section('conteudo')
     <h1>Editar Jogo</h1>
-    <form method="post" action="{{ route('jogo.update',$jogo) }}" enctype="multipart/form-data" >
-        @method('PUT')
-        @component('app.jogo._components.form_create_edit_jogo',['generos' => $generos,'produtoras' => $produtoras,'jogo' => $jogo])
-        @endcomponent
-    </form>
-
+    <div class="formulario-container ">
+        <form method="post" action="{{ route('jogo.update',$jogo) }}" enctype="multipart/form-data" >
+            @method('PUT')
+            @component('app.jogo._components.form_create_edit_jogo',['generos' => $generos,'produtoras' => $produtoras,'jogo' => $jogo])
+            @endcomponent
+        </form>
+    </div>
 @endsection
